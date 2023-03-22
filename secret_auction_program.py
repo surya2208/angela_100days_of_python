a9c12eadcbdd1603d8e9c25268622770605a7819
surya2208 <suryaforwin@gmail.com>
2023-03-22 13:35:12 +0530

    adding winner =""

--- secret_auction_program.py
+++ secret_auction_program.py
@@ -12,6 +12,7 @@ bidding_finished = False
 
 def find_highest_bidder(bidding_record):
     highest_bid = 0
+    winner = ""
     for bidder in bidding_record:
         bid_amount = bidding_record[bidder]
         if bid_amount > highest_bid:
